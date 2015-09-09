8554d2e7a7f8c1c1793f6b1a523fec8c6624dddd
wiber <elias@youiest.com>
2015-09-09 21:48:45 +0400

    gorgeous shell script

--- start.sh
+++ start.sh
@@ -1,13 +1,33 @@
+
+# this function is called when Ctrl-C is sent
+function trap_ctrlc ()
+{
+   # perform cleanup here
+   echo "Ctrl-C caught...performing clean up"
+   pkill -f meteor
+   echo "Doing cleanup"
+
+   # exit shell script with error code 2
+   # if omitted, shell script will continue execution
+   exit 2
+}
+
+# initialise trap to call trap_ctrlc function
+# when signal 2 (SIGINT) is received
+trap "trap_ctrlc" 2
+
+
+
 cd main
-meteor add meteorhacks:cluster
+meteor add meteorhacks:cluster &
 meteor --port 6001 > ../main.log &
 cd ..
 cd search
-meteor add meteorhacks:cluster
+meteor add meteorhacks:cluster &
 meteor --port 7001 > ../search.log &
 cd ..
 cd logging
-meteor add meteorhacks:cluster
+meteor add meteorhacks:cluster &
 meteor --port 8001 > ../logging.log &
 cd ..
 tail -f ./main.log

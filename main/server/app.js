18b6807d44d29fd7c7fdf89703f58ff67b164507
wiber <elias@youiest.com>
2015-09-09 21:38:37 +0400

    crosseyed error

--- main/server/app.js
+++ main/server/app.js
@@ -1,8 +1,20 @@
-SearchSource.defineSource('packages', function(searchText, options) {
-  return [];
-});
 
 Meteor.publish('topPackages', function() {
   var options = {sort: {isoScore: -1}, limit: 20};
   return Packages.find({}, options);
+});
+// connecting to the logging service
+var logConn = DDP.connect("http://localhost:8001");
+// connecting to the search service
+var searchConn = DDP.connect("http://localhost:7001");
+
+SearchSource.defineSource('packages', function(searchText, options) {
+  // we don't need to wait for completing the log method call
+  // that's why we defer it's execution and run in the next event loop
+  Meteor.defer(function() {
+    logConn.call('log', 'main-app', "searching for: " + searchText);
+  });
+
+  var searchResult = searchConn.call("getPackages", searchText, options);
+  return searchResult;
 });
--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 cd main
 meteor --port 6001 > ../main.log &
 cd ..
-cd logging
-meteor --port 7001 > ../logging.log &
-cd ..
 cd search
-meteor --port 8001 > ../search.log &
+meteor --port 7001 > ../search.log &
+cd ..
+cd logging
+meteor --port 8001 > ../logging.log &
 cd ..
 tail -f ./main.log

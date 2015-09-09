a219aa4e99063dbe1819d14d9bbc9bd85288ddaf
wiber <elias@youiest.com>
2015-09-09 21:40:55 +0400

    lazy accountability of automation

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 cd main
+meteor add meteorhacks:cluster
 meteor --port 6001 > ../main.log &
 cd ..
 cd search
+meteor add meteorhacks:cluster
 meteor --port 7001 > ../search.log &
 cd ..
 cd logging
+meteor add meteorhacks:cluster
 meteor --port 8001 > ../logging.log &
 cd ..
 tail -f ./main.log
